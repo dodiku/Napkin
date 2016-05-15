738c59110c8d534f083c28d6774c3e18621de4a4
dodiku <ayalon.dror@gmail.com>
2016-05-15 18:41:38 +0300

    static home page

--- napkin_project/napkin/views.py
+++ napkin_project/napkin/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("this is django on napkin")
+    context_dict = {
+    'maintitle': "NAPKIN is a place where you can share content with a closed group of people.",
+    'subtitle': "No registration. No signup."
+    }
+
+    return render(request, 'napkin/index.html', context_dict)
--- napkin_project/napkin_project/settings.py
+++ napkin_project/napkin_project/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'napkin_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
