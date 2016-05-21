179315afb49061594d04567791b1af44f1930513
dodiku <ayalon.dror@gmail.com>
2016-05-22 02:16:04 +0300

    getting group initiation date on group_page.html

--- napkin_project/napkin/views.py
+++ napkin_project/napkin/views.py
@@ -5,6 +5,7 @@ from napkin.forms import GroupForm, PostForm
 from napkin.models import Group, Post
 from django.template.defaultfilters import slugify
 import datetime
+from time import strftime
 
 def index(request):
 
@@ -47,16 +48,26 @@ def group_page(request, group_name_slug):
     try:
         group_object = Group.objects.get(name_slug=group_name_slug)
         group_id = group_object.id
+        print (group_id)
+        # pprint.pprint (group_object)
+        print (Group.objects.filter(name_slug=group_name_slug).values())
         group_name = group_object.name
+        print (group_name)
         # Post.objects.annotate(post_date=Min('created')).filter(group_id=group_id)
         # first_post = Post.objects.filter(group_id=group_id).first()
         # first_post_date = first_post.created
 
-        posts = Post.objects.filter(group_id=group_id).order_by('created')
-        if posts == []:
+        posts = Post.objects.filter(group_id=group_id).order_by('-created')
+        print "post object --"
+        print (Post.objects.filter(group_id=group_id).order_by('-created').values())
+        print "post object datetime.now() --"
+        print (datetime.datetime.now())
+        if not posts:
             first_post_date = "This Napkin is empty."
-        # else:
-        #     first_post_date = posts[0].created
+        else:
+            first_post = Post.objects.filter(group_id=group_id).order_by('-created').first()
+            first_post_date_unformated = first_post.created
+            first_post_date = "Since " + first_post_date_unformated.strftime("%B %d, %Y")
 
         if request.method == 'POST':
             form = PostForm(request.POST)
@@ -71,7 +82,7 @@ def group_page(request, group_name_slug):
         else:
             context_dict = {
             'group_name': group_name,
-            # 'created_date': first_post_date,
+            'created_date': first_post_date,
             'posts': posts,
             'form': PostForm(),
             }
