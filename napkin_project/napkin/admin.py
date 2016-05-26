8ba0361d644d63721b1b235cf30586609ec527bc
dodiku <ayalon.dror@gmail.com>
2016-05-27 02:50:47 +0300

    updated admin. don't know why.

--- napkin_project/napkin/admin.py
+++ napkin_project/napkin/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from napkin.models import Group, Post
 
-admin.site.register(Group)
-admin.site.register(Post)
 class GroupAdmin (admin.ModelAdmin):
     list_display = ('id', 'name', 'name_slug', 'created')
 
 class PostAdmin (admin.ModelAdmin):
+    list_display = ('id', 'group_name', 'url', 'text', 'created')
 
+    def group_name(self, obj):
+        return obj.group.name
+    group_name.short_description = 'Group Name'
+    group_name.admin_order_field = 'group'
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Post, PostAdmin)
