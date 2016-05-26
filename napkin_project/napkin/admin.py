241eae185125dfe0a5240193fa0fff03891f6b2d
dodiku <ayalon.dror@gmail.com>
2016-05-27 02:50:18 +0300

    updated admin. don't know why.

--- napkin_project/napkin/admin.py
+++ napkin_project/napkin/admin.py
@@ -3,3 +3,8 @@ from napkin.models import Group, Post
 
 admin.site.register(Group)
 admin.site.register(Post)
+class GroupAdmin (admin.ModelAdmin):
+    list_display = ('id', 'name', 'name_slug', 'created')
+
+class PostAdmin (admin.ModelAdmin):
+
