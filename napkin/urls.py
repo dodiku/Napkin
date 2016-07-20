b745300b6d1f25253e7cf2f9688b70831437e487
dodiku <ayalon.dror@gmail.com>
2016-07-20 02:56:13 -0400

    added admin + update to gitignore

--- napkin/urls.py
+++ napkin/urls.py
@@ -3,8 +3,8 @@ from napkin import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    # url(r'^admin/', admin.site.urls),
     url(r'^feedback/$', views.feedback, name="feedback"),
     url(r'^about/$', views.about, name="about"),
     url(r'^(?P<group_name_slug>[\w\-]+)/$', views.group_page, name="group_page"),
+    url(r'^admin/admin/admin', admin.site.urls),
     ]
