1b80bf17dd1733d00b76a9c1b75ee268e1ff6494
dodiku <ayalon.dror@gmail.com>
2016-07-20 01:59:07 -0400

    changed wsgi file according to heroku's tutorial

--- napkin_project/settings.py
+++ napkin_project/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
