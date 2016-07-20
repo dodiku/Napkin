5503c01a65a742d66ab26ec4f9ed838764ebd7ff
dodiku <ayalon.dror@gmail.com>
2016-07-20 04:14:45 -0400

    changing domains

--- napkin/views.py
+++ napkin/views.py
@@ -30,7 +30,7 @@ def index(request):
         name_slug = slugify(name)
         print name_slug
         if Group.objects.filter(name_slug=name_slug).count() > 0 and name != "":
-            redirection_url = "https://napkin-project.herokuapp.com/"+name_slug
+            redirection_url = "http://www.thisisnapkin.com/"+name_slug
             return redirect(redirection_url)
         else:
             if form.is_valid():
@@ -43,7 +43,7 @@ def index(request):
                 group.name_slug = slugify(group.name)
                 group.created = datetime.datetime.now()
                 group.save()
-                redirection_url = "https://napkin-project.herokuapp.com/"+group.name_slug
+                redirection_url = "http://www.thisisnapkin.com/"+group.name_slug
                 return redirect(redirection_url)
 
             else:
@@ -103,7 +103,7 @@ def group_page(request, group_name_slug):
 
                     post.save()
 
-                redirection_url = "https://napkin-project.herokuapp.com/"+group_name_slug
+                redirection_url = "http://www.thisisnapkin.com/"+group_name_slug
                 return redirect(redirection_url)
 
             else:
@@ -118,7 +118,7 @@ def group_page(request, group_name_slug):
             name_slug = slugify(name)
             print name_slug
             if Group.objects.filter(name_slug=name_slug).count() > 0 and name != "":
-                redirection_url = "https://napkin-project.herokuapp.com/"+name_slug
+                redirection_url = "http://www.thisisnapkin.com/"+name_slug
                 return redirect(redirection_url)
             else:
                 if form.is_valid():
@@ -131,7 +131,7 @@ def group_page(request, group_name_slug):
                     group.name_slug = slugify(group.name)
                     group.created = datetime.datetime.now()
                     group.save()
-                    redirection_url = "https://napkin-project.herokuapp.com/"+group.name_slug
+                    redirection_url = "http://www.thisisnapkin.com/"+group.name_slug
                     return redirect(redirection_url)
 
                 else:
